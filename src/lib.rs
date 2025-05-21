719d067cccefc8d9455fcfb29bf4e31318059281
Stan-breaks <stanleymwendwa03@gmail.com>
2025-05-21 18:08:51 +0300

    process record init

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
+use byteorder::LittleEndian;
 use serde::{Deserialize, Serialize};
 use std::{
     collections::HashMap,
     fs::{File, OpenOptions},
-    io::{self, BufReader, Seek, SeekFrom},
+    io::{self, BufReader, Read, Seek, SeekFrom},
     path::Path,
 };
 
@@ -35,7 +36,22 @@ impl ActionKV {
         let mut f = BufReader::new(&mut self.f);
         loop {
             let position = f.seek(SeekFrom::Current(0))?;
+            let maybe_kv = ActionKV::process_record(&mut f);
         }
         Ok(())
+    }
+    fn process_record<R: Read>(f: &mut R) -> io::Result<KeyValuePair> {
+        let mut buffer = [0; 4];
+        f.read_exact(&mut buffer).expect("failed to read checksum");
+        let saved_checksum = u32::from_le_bytes(buffer);
+        f.read_exact(&mut buffer);
+        let key_len = u32::from_le_bytes(buffer);
+        f.read_exact(&mut buffer);
+        let val_len = u32::from_le_bytes(buffer);
+        let data_len = key_len + val_len;
+        Ok(KeyValuePair {
+            key: key,
+            value: value,
+        })
     }
 }
