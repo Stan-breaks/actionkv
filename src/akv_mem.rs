313a07d04ebd7cadeda4c763964c586d461be78a
Stan-breaks <stanleymwendwa03@gmail.com>
2025-05-21 16:22:48 +0300

    cfg flags

--- src/akv_mem.rs
+++ src/akv_mem.rs
@@ -1,3 +1,25 @@
+#[cfg(target_os = "windows")]
+const USAGE: &str = "
+USAGE:
+     akv_mem.exe FILE get KEY
+     akv_mem.exe FILE delete KEY
+     akv_mem.exe FILE insert KEY VALUE
+     akv_mem.exe FILE update KEY VALUE
+";
+
+#[cfg(not(target_os = "windows"))]
+const USAGE: &str = "
+USAGE:
+     akv_mem FILE get KEY
+     akv_mem FILE delete KEY
+     akv_mem FILE insert KEY VALUE
+     akv_mem FILE update KEY VALUE
+";
+
 fn main() {
-    println!("Hello, World!");
+    let args: Vec<String> = std::env::args().collect();
+    let fname = args.get(1).expect(&USAGE);
+    let action: &str = args.get(2).expect(&USAGE).as_ref();
+    let key: &str = args.get(3).expect(&USAGE).as_ref();
+    let maybe_value = args.get(4);
 }
