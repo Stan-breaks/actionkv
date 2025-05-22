1920968a80adde7dffcfa721a38aed33d49c4ae5
Stan-breaks <stanleymwendwa03@gmail.com>
2025-05-22 18:04:40 +0300

    bytestr type

--- src/akv_mem.rs
+++ src/akv_mem.rs
@@ -1,9 +1,11 @@
+use std::{env::args, path::Path};
+
 use actionkv::ActionKV;
 
 #[cfg(target_os = "windows")]
 const USAGE: &str = "
 USAGE:
-     akv_mem.exe FILE get KEY
+     akv_mem
      akv_mem.exe FILE delete KEY
      akv_mem.exe FILE insert KEY VALUE
      akv_mem.exe FILE update KEY VALUE
@@ -18,31 +20,28 @@ USAGE:
      akv_mem FILE update KEY VALUE
 ";
 
+type Bytestr = [u8];
+
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let args: Vec<String> = args().collect();
     let fname = args.get(1).expect(&USAGE);
     let action: &str = args.get(2).expect(&USAGE).as_ref();
-    let key: &str = args.get(3).expect(&USAGE).as_ref();
+    let key: &Bytestr = args.get(3).expect(&USAGE).as_ref();
     let maybe_value = args.get(4);
 
-    let path = std::path::Path::new(&fname);
+    let path = Path::new(&fname);
     let mut store = ActionKV::open(path).expect("unable to open file");
     store.load().expect("unable to load data");
 
     match action {
-        "get" => match store.index.get(key).unwrap() {
-            None => eprintln!("{:?} not found", key),
+        "get" => match store.get(key).unwrap() {
             Some(value) => println!("{:?}", value),
+            None => eprintln!("unable to get value of key: {:?}", key),
         },
-        "delete" => store.index.remove(key),
         "insert" => {
             let value = maybe_value.expect(&USAGE).as_ref();
-            store.index.insert(key, value)
-        }
-        "update" => {
-            let value = maybe_value.expect(&USAGE).as_ref();
-            store.index.insert(key, value).unwrap()
+            store.insert(key, value).unwrap()
         }
         _ => eprintln!("{}", &USAGE),
-    }
+    };
 }
